6558fcbeebd159cec6503549d7bc9e2a3ce0d6b4
sirisha469 <chsiri469@gmail.com>
2023-06-15 16:32:11 +0530

    first-file

---
+++ Prime_06.java
@@ -0,0 +1,22 @@
+public class Prime_06{
+  public static void main(String[] args) {
+    System.out.println("Hello"); 
+    int num = 29;
+    int c = 2;
+
+    //System.out.println(Math.round(Math.sqrt(num)));
+    //System.out.println(Math.round(Math.pow(num,0.5)));
+    double val = Math.round(Math.sqrt(num));
+    while(c <= val){
+
+      if(num%c == 0){
+        System.out.println("Not a prime");
+        System.exit(0);
+      }
+      c+=1;
+
+    }
+    System.out.println("Prime");
+    
+  }
+}
