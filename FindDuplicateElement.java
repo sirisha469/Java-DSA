6ae046e016d91e0dd09ccc9fc747e22f7aceee43
sirisha469 <chsiri469@gmail.com>
2023-09-04 13:47:14 +0530

    duplicates-cyclic-sort

--- FindDuplicateElement.java
+++ FindDuplicateElement.java
@@ -1,24 +1,33 @@
 //https://leetcode.com/problems/find-the-duplicate-number/
+
 public class FindDuplicateElement {
   public static void main(String[] args) {
-    int[] arr = {1,3,4,2,2};
-    System.out.println(DuplicateElement(arr));
+    int[] arr = {4, 3, 2, 7, 6, 2, 1};
+    System.out.println(duplicateElement(arr));
   }
 
-  public static int DuplicateElement(int[] nums){
+  public static int duplicateElement(int[] nums){
     int i=0, len=nums.length;
     while(i < len){
-      int correctIndex = nums[i] - 1;
-      if(nums[i]!=nums[correctIndex]){
-        swap(nums, i, correctIndex);
+
+      if(nums[i]!= i+1){
+        int correctIndex = nums[i] - 1;
+        if(nums[i]!=nums[correctIndex]){
+          swap(nums, i, correctIndex);
+        }
+        else{
+          return nums[i];
+        }
       }
       else{
         i++;
       }
+     
     }
 
+    return -1;
     //last element after cyclic sort
-    return nums[len-1];
+    //return nums[len-1];
   }
 
   public static void swap(int[] nums, int i, int correctIndex) {
