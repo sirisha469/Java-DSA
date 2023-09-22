5e122e6dd3ef3d19227126f77864fed52b358864
sirisha469 <chsiri469@gmail.com>
2023-09-22 12:13:01 +0530

    recursion-leetcode

---
+++ recursion/IsSubsequence.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/is-subsequence/description/
+
+package recursion;
+
+public class IsSubsequence {
+  public static void main(String[] args) {
+    String s = "abc";
+    String t = "ahbgdac";
+
+    System.out.println(isSubsequence(s,t,0,0));
+  }
+
+  public static boolean isSubsequence(String s, String t, int i, int j){
+
+    if(s.length() == i){
+        return true;
+    }
+
+    if(s.length() > i && j == t.length())
+        return false; 
+
+    if(s.charAt(i) == t.charAt(j)){
+        return isSubsequence(s, t, i+1, j+1);
+    }
+    return isSubsequence(s, t, i, j+1);
+}
+
+}
