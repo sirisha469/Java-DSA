5927be811f25468b780c49d8e8f3798465fa5ff5
sirisha469 <chsiri469@gmail.com>
2023-09-22 12:19:43 +0530

    two-pointer

---
+++ TwoPointer/IsSubsequence.java
@@ -0,0 +1,31 @@
+package TwoPointer;
+
+public class IsSubsequence {
+  public static void main(String[] args) {
+    String s = "abc";
+    String t = "ahbgdac";
+
+    System.out.println(isSubsequence(s,t));
+  }
+
+  public static boolean isSubsequence(String s, String t){
+        
+    int i=0, j=0;
+
+    while(i<s.length() && j<t.length()){
+
+        if(s.charAt(i) == t.charAt(j)){
+            i++;
+            j++;
+        }
+        else
+            j++;
+
+    }
+    if(i == s.length())
+        return true;
+    else 
+        return false;
+
+}
+}
