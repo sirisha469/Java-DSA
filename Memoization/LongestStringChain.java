fa0ea47989b365f657b7e03fe8a00fee34e2a04f
sirisha469 <chsiri469@gmail.com>
2023-09-24 11:20:08 +0530

    string-leetcode

--- Memoization/LongestStringChain.java
+++ Memoization/LongestStringChain.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/longest-string-chain/description/
+
 package Memoization;
 
 import java.util.Arrays;
---
+++ Strings/MergeStringsAlternate.java
@@ -0,0 +1,30 @@
+package Strings;
+
+public class MergeStringsAlternate {
+  
+  public static void main(String[] args) {
+    String a = "abc";
+    String b = "pqrs";
+    //output: apbqcrs
+    System.out.println(mergeAlternately(a, b));
+  }
+
+  public static String mergeAlternately(String word1, String word2) {
+        
+    StringBuilder sb = new StringBuilder();
+
+    int i = 0;
+
+    while(i < word1.length() || i < word2.length()){
+        if(i < word1.length()){
+            sb.append(word1.charAt(i));
+        }
+        if(i < word2.length()){
+            sb.append(word2.charAt(i));
+        }
+        i++;
+    }
+    return sb.toString();
+
+  }
+}
