816271550330fb902ad5ad5e855e1368df4a24b2
sirisha469 <chsiri469@gmail.com>
2023-09-29 22:46:06 +0530

    fibonacci

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -2,8 +2,23 @@ package recursion;
 
 public class Fibonacci {
   public static void main(String[] args) {
-    int ans = fibonacci(4);
-    System.out.println(ans);
+    //int ans = fibonacci(4);
+    //System.out.println(ans);
+
+    for(int i=1;i<11;i++){
+      System.out.println(i+"-->"+fiboFormula(i));
+    }
+
+    System.out.println(fiboFormula(50));
+
+  }
+
+  public static int fiboFormula(int n){
+    double left = Math.pow((1+Math.sqrt(5))/2,n);
+    //double right = Math.pow((1-Math.sqrt(5))/2,n);
+    //less dominating term removed
+
+    return (int) (left/Math.sqrt(5));
   }
 
   static int fibonacci(int n){
