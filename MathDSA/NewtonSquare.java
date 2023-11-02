e2c1cecaf04bc791aa6d0dfc64ac237934f37551
sirisha469 <chsiri469@gmail.com>
2023-11-02 22:12:57 +0530

    math

---
+++ MathDSA/NewtonSquare.java
@@ -0,0 +1,23 @@
+package MathDSA;
+// import java.math.*;
+
+public class NewtonSquare{
+  public static void main(String[] args) {
+    System.out.println(sqrt(40));
+  }
+
+  public static double sqrt(double n){
+    double x = n;
+    double root;
+    
+    while(true){
+      root = 0.5 * (x + (n/x));
+
+      if(Math.abs(root-x) < 0.5){
+        break;
+      }
+      x = root;
+    }
+    return root;
+  }
+}
