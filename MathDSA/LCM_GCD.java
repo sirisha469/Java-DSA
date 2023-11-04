878829830fe10b43952af7b21930eab1f3ca5461
Sirisha Challagiri <chsiri469@gmail.com>
2023-11-04 20:28:20 +0530

    math

---
+++ MathDSA/LCM_GCD.java
@@ -0,0 +1,22 @@
+package MathDSA;
+
+public class LCM_GCD {
+  public static void main(String[] args) {
+    System.out.println(gcd(4, 8));
+    System.out.println(gcd(4, 9));
+
+    System.out.println(lcm(2,7));
+  }
+
+  public static int gcd(int a, int b){
+    if(a==0)
+      return b;
+    
+    return gcd( b%a, a);
+  }
+
+  //It use gcd
+  public static int lcm(int a, int b){
+    return a*b / gcd(a,b);
+  }
+}
