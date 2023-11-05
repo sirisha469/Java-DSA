58cddd24136ad933f2805d9fd0db4fea0b90e6fe
Sirisha Challagiri <chsiri469@gmail.com>
2023-11-05 17:04:58 +0530

    recursion-problems

---
+++ WinnerArrayGame.java
@@ -0,0 +1,10 @@
+
+public class WinnerArrayGame {
+  public static void main(String[] args) {
+    int[] arr = {2,1,3,5,4,6,7};
+    int k=2;
+    // System.out.println(arr,k);
+  }
+
+ 
+}
--- recursion/ReverseNumber.java
+++ recursion/ReverseNumber.java
@@ -2,18 +2,39 @@ package Recursion;
 
 public class ReverseNumber {
   public static void main(String[] args) {
-    reverse(1234);
+    reverse1(1234);
     System.out.println(sum);
+
+    System.out.println(rev(1234));
   }
 
+  //way1 
   static int sum=0;
-  public static void reverse(int n){
+  public static void reverse1(int n){
     if(n==0)
       return;
     
     sum = sum * 10 + (n%10);
 
-    reverse(n/10);
+    reverse1(n/10);
+  }
+
+  //way2
+
+  public static int rev(int n){
+    int digits = (int)(Math.log10(n)+1);
+
+    return helper(n, digits);
+  }
+
+  public static int helper(int n,int digits){
+    if(n%10 == n){
+      return n;
+    }
+
+    int rem = n%10;
+
+    return rem * (int)(Math.pow(10,digits-1)) + helper(n/10, digits-1);
   }
   
 }
