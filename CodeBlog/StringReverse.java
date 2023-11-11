d5c7bab0cec6bff94f3987d3bbb3e35f2e2b135f
Sirisha Challagiri <chsiri469@gmail.com>
2023-11-11 23:30:15 +0530

    blog-code

--- CodeBlog/StringReverse.java
+++ CodeBlog/StringReverse.java
@@ -3,36 +3,37 @@ package CodeBlog;
 public class StringReverse {
   public static void main(String[] args) {
     String s = "code with siri";
-    // String str="";
-    // for(int i=s.length()-1;i>=0;i--){
-    //   str+=s.charAt(i);
-    // }
-    // System.out.println(str);
+    String s1="";
+    for(int i=s.length()-1;i>=0;i--){
+      s1+=s.charAt(i);
+    }
+    System.out.println(s1);
 
 
-    // StringBuilder sb = new StringBuilder(s);
-    // sb.reverse();
-    // System.out.println(sb);
+    StringBuilder sb = new StringBuilder(s);
+    sb.reverse();
+    System.out.println(sb);
 
-    // String original = "Hello, World!";
-    //     char[] charArray = original.toCharArray();
-    //     int start = 0;
-    //     int end = charArray.length - 1;
-    //     while (start < end) {
-    //         // Swap characters at start and end indices
-    //         char temp = charArray[start];
-    //         charArray[start] = charArray[end];
-    //         charArray[end] = temp;
-    //         // Move indices towards the center
-    //         start++;
-    //         end--;
-    //     }
-    //     String reversed = new String(charArray);
-    //     System.out.println("Original: " + original);
-    //     System.out.println("Reversed: " + reversed);
+    String original = "Hello, World!";
+        char[] charArray = original.toCharArray();
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            // Swap characters at start and end indices
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+            // Move indices towards the center
+            start++;
+            end--;
+        }
+        String reversed = new String(charArray);
+        System.out.println("Original: " + original);
+        System.out.println("Reversed: " + reversed);
 
-      char[] str = s.toCharArray();
-    for(int i=0,j=s.length()-1;i<s.length()/2;i++,j--){
+    char[] str = s.toCharArray();
+    int len = str.length;
+    for(int i=0,j=len-1;i<len/2;i++,j--){
       char temp = str[i];
       str[i] = str[j];
       str[j] = temp;
---
+++ CodeBlog/Swap.java
@@ -0,0 +1,17 @@
+package CodeBlog;
+public class Swap {
+  public static void main(String[] args) {
+    int a=10,b=20;
+    System.out.println("Before swapping: a="+a+" b="+b);
+    int temp = a;
+    a = b;
+    b=temp;
+
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    System.out.println("After swapping: a="+a+" b="+b);
+    
+
+  }
+}
