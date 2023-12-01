7c8db7fe7ac5b9ca428aa89495667ad13105bcfa
Sirisha Challagiri <chsiri469@gmail.com>
2023-12-01 14:31:57 +0530

    blog

--- CodeBlog/ValidPalindrome.java
+++ CodeBlog/ValidPalindrome.java
@@ -1,13 +1,15 @@
 package CodeBlog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidPalindrome {
   public static void main(String[] args) {
     String s = "A man, a plan, a canal: Panama";
-    System.out.println("Is valid palindrome: "+isPalindrome(s));
-
+    System.out.println("Is valid palindrome: "+isPalindrome1(s));
   }
+
   public static boolean isPalindrome(String s) {
-        
     int start =0,last =s.length()-1;
     while(start < last){
       char currFirst = s.charAt(start);
@@ -26,6 +28,28 @@ public class ValidPalindrome {
         last--;
       }
     }
+    return true;
+  }
+
+  public static boolean isPalindrome1(String s) {
+    List<Character> list = new ArrayList<>();
+
+    s = s.toLowerCase();
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (Character.isLetterOrDigit(c)) {
+        list.add(c);
+      }
+    }
+    
+    //2.check if Palindrome or not
+    for (int i = 0; i < list.size() / 2; i++) {
+      if (list.get(i) != list.get(list.size() - 1 - i)) {
+        return false;
+      }
+    }
+
     return true;
   }
 }
