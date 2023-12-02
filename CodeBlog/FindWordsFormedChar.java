39c1f37770201e446fadaff8bdd7774b07bbe6d6
Sirisha Challagiri <chsiri469@gmail.com>
2023-12-02 20:51:12 +0530

    blog

--- CodeBlog/FindWordsFormedChar.java
+++ CodeBlog/FindWordsFormedChar.java
@@ -11,27 +11,27 @@ public class FindWordsFormedChar {
     int[] charsArr = new int[26];
 
     for(int i=0;i<chars.length();i++){
-        charsArr[chars.charAt(i)-'a']++;
+      charsArr[chars.charAt(i)-'a']++;
     }
     int sum=0;
     for(String s:words){
-        if(check(s,charsArr)){
-            sum+=s.length();
-        }
+      if(check(s,charsArr)){
+        sum+=s.length();
+      }
     }
     return sum;
-}
+  }
 
-public static boolean check(String s,int[] charsArr){
+  public static boolean check(String s,int[] charsArr){
     int[] wordArr = new int[26];
 
     for(int i=0;i<s.length();i++){
-        int m = s.charAt(i)-'a';
-        wordArr[m]++;
-        if(wordArr[m] > charsArr[m]){
-            return false;
-        }
+      int m = s.charAt(i)-'a';
+      wordArr[m]++;
+      if(wordArr[m] > charsArr[m]){
+        return false;
+      }
     }
     return true;
-}
+  }
 }
