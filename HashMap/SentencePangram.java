d614f4322e9ac5c8450b26156a25f56064955d4c
Sirisha Challagiri <chsiri469@gmail.com>
2023-12-12 20:36:44 +0530

    pangram

--- HashMap/SentencePangram.java
+++ HashMap/SentencePangram.java
@@ -15,14 +15,14 @@ public class SentencePangram {
   public static boolean pangram(String s){
     HashMap<Character,Integer> map=new HashMap<>();
 
-        for(int i=0;i<s.length();i++){
-            map.put(s.charAt(i),1);
-        }
+    for(int i=0;i<s.length();i++){
+      map.put(s.charAt(i),1);
+    }
 
-        if(map.size() == 26){
-            return true;
-        }
-        return false;
+    if(map.size() == 26){
+      return true;
+    }
+    return false;
   }
 
   //second simple method
