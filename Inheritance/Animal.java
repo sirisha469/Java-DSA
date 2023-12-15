74f414c7e94f96ec3a0852e85ebf0794206c0f46
Sirisha Challagiri <chsiri469@gmail.com>
2023-12-15 10:50:46 +0530

    subarraySum-gfg

--- Inheritance/Animal.java
+++ Inheritance/Animal.java
@@ -1,33 +1,45 @@
 package Inheritance;
-
-class Animal {
-  
-  static {
-    System.out.println("static Animal");
-  }
-
-  {
-    System.out.println("block animal");
-  }
-}
-
 class Bird extends Animal{
-  static {
-    System.out.println("static Bird");
+  Bird(){
+    // super(10);
+    System.out.println("Bird class constructor");
   }
-  {
-    System.out.println("Block Bird");
-  }
-
+  // static {
+  //   System.out.println("static Bird");
+  // }
+  // {
+  //   System.out.println("Block Bird");
+  // }
   void walk(){
+    super.walk();
     System.out.println("Bird walking");
   }
  
 }
+class Animal {
+  Animal(){
+    System.out.println("Animal class constructor");
+  }
+  
+  // static {
+  //   System.out.println("static Animal");
+  // }
+
+  // {
+  //   System.out.println("block animal");
+  // }
+  void walk(){
+    System.out.println("Animal walking");
+  }
+}
+
+
 
 class Main{
   public static void main(String[] args){
     Bird b = new Bird();
-    b.walk();
+    // Animal a = new Animal();
+    // a.walk();
+    //b.walk();
   }
 }
