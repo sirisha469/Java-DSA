2ca9db633b19dffb7fd8886819f5ac2087bc28d6
Sirisha Challagiri <chsiri469@gmail.com>
2024-02-27 20:56:47 +0530

    subsequence

--- recursion/String/SubSequence.java
+++ recursion/String/SubSequence.java
@@ -1,8 +1,12 @@
 package Recursion.String;
+
+import java.util.ArrayList;
+
 public class SubSequence {
   public static void main(String[] args) {
     String s = "abc";
     subSequence("", s);
+    System.out.println(subSeqRet("", s));
   }
 
   public static void subSequence(String ans,String s){
@@ -14,6 +18,21 @@ public class SubSequence {
     subSequence(ans+ch, s.substring(1));
     subSequence(ans, s.substring(1));
 
+  }
+
+  public static ArrayList<String> subSeqRet(String p,String up){
+    if(up.isEmpty()){
+      ArrayList<String> list = new ArrayList<>();
+      list.add(p);
+      return list;
+    }
+
+    char ch = up.charAt(0);
+    ArrayList<String> left = subSeqRet(p + ch, up.substring(1));
+    ArrayList<String> right = subSeqRet(p, up.substring(1));
+
+    left.addAll(right);
+    return left;
   }
 
 }
