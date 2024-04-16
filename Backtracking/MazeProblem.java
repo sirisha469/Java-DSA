73b0194d4dec5ea391f858426cc1cec80bf215eb
Sirisha Challagiri <chsiri469@gmail.com>
2024-04-16 07:16:38 +0530

    backtracking-maze

---
+++ Backtracking/MazeProblem.java
@@ -0,0 +1,18 @@
+package Backtracking;
+
+public class MazeProblem{
+  public static void main(String[] args) {
+    System.out.println("Number of paths: "+maze(4,4));
+  }
+
+  public static int maze(int r,int c){
+    if(r==1 || c==1){
+      return 1;
+    }
+
+    int left = maze(r-1,c);
+    int right = maze(r,c-1);
+
+    return left+right;
+  }
+}
